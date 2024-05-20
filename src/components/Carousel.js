d29b8c139c21a994364784c2b5606c49878c8f24
Ben Cyna <benjamincyna@gmail.com>
2024-05-20 12:49:01 -0400

    added row for going through the months

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react';
 import data from "./data.json"
 
 
-const images = [
-  
-];
 
 const Carousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [images, setImages] = useState([]);
+
+    const favourites = []
+    // use index of favouties 
+    // then in html loop through this array
+    // display the date from the data indexed
+    // onclick, set the current index to the index in array
 
     useEffect(() => {
       // Dynamically import images based on the paths in data.json
@@ -37,6 +40,18 @@ const Carousel = () => {
 
     return (
       <div className="relative w-full h-screen flex flex-col items-center justify-center mt-10">
+         <div className="fixed top-0 w-full flex justify-center bg-gray-100 bg-pink-400">
+          <div className="w-full max-w-screen-xl grid grid-cols-10 border-b border-gray-200">
+            {Array.from({ length: 10 }).map((_, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-center h-12 border-r border-white text-black hover:text-white hover:bg-black transition duration-300 ease-in-out  cursor-pointer"
+              >
+                Column {index + 1}
+              </div>
+            ))}
+          </div>
+        </div>
       <div className="flex items-center justify-center w-full relative">
         <button
           className="absolute left-10 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
@@ -48,7 +63,7 @@ const Carousel = () => {
           <img
             src={images[currentIndex]}
             alt="carousel"
-            className="rounded-lg shadow-lg max-w-full max-h-96 transition duration-500 ease-in-out transform hover:scale-110 cursor-pointer"
+            className="rounded-lg shadow-lg max-w-full max-h-96 transition duration-500 ease-in-out transform hover:scale-110"
           />
           <p className="mt-4 text-center">{data[currentIndex].date}</p>
         </div>

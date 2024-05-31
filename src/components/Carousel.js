8027de8b1a4d1e3670ab1752f8e12d1ac9771f7c
Ben Cyna <benjamincyna@gmail.com>
2024-05-30 21:24:49 -0400

    completed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -42,11 +42,11 @@ const Carousel = () => {
     return (
       <div className="relative w-full h-screen flex flex-col items-center justify-center mt-10">
         <div className="fixed top-0 w-full flex justify-center bg-pink-300">
-          <div className="w-full max-w-screen-xl grid grid-cols-5 md:grid-cols-5 lg:grid-cols-10 border-b border-gray-200">
+          <div className="w-full max-w-screen-xl grid grid-cols-5 md:grid-cols-5 lg:grid-cols-10 border-b border-gray-200 cursor-pointer">
             {favourites.map((item, index) => (
               <div
                 key={index}
-                onClick={() => setCurrentIndex(index)}
+                onClick={() => setCurrentIndex(item)}
                 className="flex items-center justify-center h-12 border-r border-white text-black hover:text-white hover:bg-black transition duration-300 ease-in-out overflow-hidden whitespace-nowrap"
               >
                 <span className="truncate px-2">{data[item].date}</span>
@@ -67,7 +67,7 @@ const Carousel = () => {
             alt="carousel"
             className="rounded-lg shadow-lg max-w-full max-h-96 transition duration-500 ease-in-out transform hover:scale-110"
           />
-          <p className="mt-4 text-center">{data[currentIndex].date}</p>
+          <p className="mt-4 text-center">{currentIndex+1}. {data[currentIndex].date}</p>
         </div>
         <button
           className="absolute right-10 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
